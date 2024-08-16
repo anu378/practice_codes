894a610e02a2664678532429d1ed3bc72d4a3b92
unknown <pratapanuj378@gmail.com>
2024-08-17 00:32:39 +0530

    transpose

---
+++ transpose.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class transpose {
+    public static void main(String[] s){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int matrix[][]= new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+}
+for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        
+        System.out.print(matrix[j][i]+" ");
+    }System.out.println();
+}
+    }
+    
+}
